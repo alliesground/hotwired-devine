1e5a1453b6ec584a18425b75034c30cfc95c8188
Sameer <sameer.libang@gmail.com>
2024-04-03 23:19:09 +1100

    adding project tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_115820) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_121537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,4 +27,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_115820) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  add_foreign_key "tasks", "projects"
 end
